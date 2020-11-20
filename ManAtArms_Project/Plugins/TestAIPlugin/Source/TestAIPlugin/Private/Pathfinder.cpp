0163ce51569d04f4ba59dc9cb198a6ef61570e45
HAETAE0011 <john0011@naver.com>
2020-11-20 13:00:34 -0500

    BP_Salmon has problems

--- ManAtArms_Project/Plugins/TestAIPlugin/Source/TestAIPlugin/Private/Pathfinder.cpp
+++ ManAtArms_Project/Plugins/TestAIPlugin/Source/TestAIPlugin/Private/Pathfinder.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+// https://www.youtube.com/playlist?list=PLHJDrb2QPWtaxCrBaH1Ht00Trv4gH_lYB
 
 #include "Pathfinder.h"
 #include "Runtime/Engine/Classes/Kismet/KismetSystemLibrary.h"
